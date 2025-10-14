d9c0aebc1f49ed59d2929afe5202858b187efd9c
IT-ankhbayar <itankhbayar@gmail.com>
2025-10-14 17:51:09 +0800

    errorfix15

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+// app/not-found.tsx
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">404 - Page Not Found</h1>
+      <p>Хуудас олдсонгүй.</p>
+    </div>
+  );
+}
